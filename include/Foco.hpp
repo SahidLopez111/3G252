01e266c992cd0c767f3480bed8d6597d4f052af5
SahidLopez111 <a24110183@live.ceti.mx>
2025-09-06 01:03:48 +0000

    Practica 1

---
+++ include/Foco.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <EstadoFoco.hpp>
+class Foco
+{
+private:
+    EstadoFoco estado;
+
+public:
+    Foco(/* args */)
+    {
+    }
+    ~Foco()
+    {
+    }
+    void Encender()
+    {
+    }
+    void Apagar()
+    {
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <Foco.hpp>
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+
+    Foco foquito;
+    foquito.Apagar();
+    foquito.Encender();
+
+    return 0;
+}
